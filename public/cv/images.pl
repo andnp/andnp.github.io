9709eb0a1fe1a284013557e5cfbbe54181e90405
andy <andnpatt@indiana.edu>
2017-04-27 19:20:42 -0400

    update cv route

---
+++ public/cv/images.pl
@@ -0,0 +1,96 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate images original text with physical files.
+
+
+$key = q/textstyleparbox{textwidth}{setlength{FrameSep}{outerbordwidth}shaded{setlength{fbox[6.762in][l]{largePublications}vphantom{p^{E}}}}}}shaded{};MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="810" HEIGHT="90" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img5.png"
+ ALT="$\textstyle \parbox{\textwidth}{\setlength{\FrameSep}{\outerbordwidth}
+\begin{...
+...kebox[6.762in][l]{\large Publications} \vphantom{p\^{E}}}}}}
+\end{shaded}
+}$">|; 
+
+$key = q/textstyleparbox{textwidth}{setlength{FrameSep}{outerbordwidth}shaded{setlength{fs{mdseries(github.comslashandnp)}}vphantom{p^{E}}}}}}shaded{};MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="810" HEIGHT="92" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img8.png"
+ ALT="$\textstyle \parbox{\textwidth}{\setlength{\FrameSep}{\outerbordwidth}
+\begin{...
+...rojects {\mdseries(github.com/andnp)}} \vphantom{p\^{E}}}}}}
+\end{shaded}
+}$">|; 
+
+$key = q/textstyleparbox{textwidth}{setlength{FrameSep}{outerbordwidth}shaded{setlength{f2in][l]{largeAwards,Grants&Honors}vphantom{p^{E}}}}}}shaded{};MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="810" HEIGHT="90" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img7.png"
+ ALT="$\textstyle \parbox{\textwidth}{\setlength{\FrameSep}{\outerbordwidth}
+\begin{...
+...n][l]{\large Awards, Grants \&amp; Honors} \vphantom{p\^{E}}}}}}
+\end{shaded}
+}$">|; 
+
+$key = q/textstyleparbox{textwidth}{setlength{FrameSep}{outerbordwidth}shaded{setlength{f762in][l]{largeTeachingExperience}vphantom{p^{E}}}}}}shaded{};MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="810" HEIGHT="90" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="$\textstyle \parbox{\textwidth}{\setlength{\FrameSep}{\outerbordwidth}
+\begin{...
+....762in][l]{\large Teaching Experience} \vphantom{p\^{E}}}}}}
+\end{shaded}
+}$">|; 
+
+$key = q/textstyleparbox{textwidth}{setlength{FrameSep}{outerbordwidth}shaded{setlength{f~}makebox[6.762in][l]{largeSkills}vphantom{p^{E}}}}}}shaded{};MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="810" HEIGHT="90" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img9.png"
+ ALT="$\textstyle \parbox{\textwidth}{\setlength{\FrameSep}{\outerbordwidth}
+\begin{...
+...{&nbsp;}\makebox[6.762in][l]{\large Skills} \vphantom{p\^{E}}}}}}
+\end{shaded}
+}$">|; 
+
+$key = q/textstyleparbox{textwidth}{setlength{FrameSep}{outerbordwidth}shaded{setlength{f762in][l]{largeIndustryExperience}vphantom{p^{E}}}}}}shaded{};MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="810" HEIGHT="90" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img4.png"
+ ALT="$\textstyle \parbox{\textwidth}{\setlength{\FrameSep}{\outerbordwidth}
+\begin{...
+....762in][l]{\large Industry Experience} \vphantom{p\^{E}}}}}}
+\end{shaded}
+}$">|; 
+
+$key = q/textstyleparbox{textwidth}{setlength{FrameSep}{outerbordwidth}shaded{setlength{fox[6.762in][l]{largePresentations}vphantom{p^{E}}}}}}shaded{};MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="810" HEIGHT="90" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img6.png"
+ ALT="$\textstyle \parbox{\textwidth}{\setlength{\FrameSep}{\outerbordwidth}
+\begin{...
+...ebox[6.762in][l]{\large Presentations} \vphantom{p\^{E}}}}}}
+\end{shaded}
+}$">|; 
+
+$key = q/textstyleparbox{textwidth}{setlength{FrameSep}{outerbordwidth}shaded{setlength{fakebox[6.762in][l]{largeEducation}vphantom{p^{E}}}}}}shaded{};MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="810" HEIGHT="90" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="$\textstyle \parbox{\textwidth}{\setlength{\FrameSep}{\outerbordwidth}
+\begin{...
+...\makebox[6.762in][l]{\large Education} \vphantom{p\^{E}}}}}}
+\end{shaded}
+}$">|; 
+
+$key = q/textstyleparbox{textwidth}{setlength{FrameSep}{outerbordwidth}shaded{setlength{f762in][l]{largeResearchExperience}vphantom{p^{E}}}}}}shaded{};MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="810" HEIGHT="90" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="$\textstyle \parbox{\textwidth}{\setlength{\FrameSep}{\outerbordwidth}
+\begin{...
+....762in][l]{\large Research Experience} \vphantom{p\^{E}}}}}}
+\end{shaded}
+}$">|; 
+
+1;
+
