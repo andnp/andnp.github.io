28dfc7731ea39da7060c732ea2cc6ba322b6c45a
andy <andnpatterson@gmail.com>
2020-10-25 11:50:08 -0600

    update scholar checking script

--- scripts/commit_scholar.sh
+++ scripts/commit_scholar.sh
@@ -3,4 +3,10 @@ npm run publications
 git checkout -b Publication
 
 git add src/content/publications.ts
-CHANGED=$(git diff-index --quiet --cached HEAD) || git commit -m "updating papers"
+CHANGED=$(git diff-index --quiet --cached HEAD)
+
+if [ -z $CHANGED ]; then
+  echo "hey there"
+  git push --set-upstream origin Publication
+  curl -H "Authorization: token $1" -X POST -d '{ "title": "Add Publication", "head": "Publication", "base": "develop" }' "https://api.github.com/repos/andnp/andnp.github.io/pulls" -H "Content-Type:application/json"
+fi
