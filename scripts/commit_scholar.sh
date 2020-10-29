10b0ee81af7dc0a473d0c7f8ec6e3a36ef385135
Andy Patterson <andnpatterson@gmail.com>
2020-10-29 10:10:38 -0600

    Update commit_scholar.sh

--- scripts/commit_scholar.sh
+++ scripts/commit_scholar.sh
@@ -6,6 +6,8 @@ git add src/content/publications.ts
 CHANGED=$(git diff-index --quiet --cached HEAD)
 
 if [ -z $CHANGED ]; then
+  git config user.name "andnp"
+  git config user.email "andnpatterson@gmail.com"
   git commit -m "add new publication!"
   git push --set-upstream origin Publication
   curl -H "Authorization: token $1" -X POST -d '{ "title": "Add Publication", "head": "Publication", "base": "develop" }' "https://api.github.com/repos/andnp/andnp.github.io/pulls" -H "Content-Type:application/json"
